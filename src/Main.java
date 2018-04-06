7e6cf0d7dd3c46210c3fa2f4cdc6a5f0e17f8a6f
GregoireSelle <36163756+GregoireSelle@users.noreply.github.com>
2018-04-06 11:57:33 +0200

    test de librairie graphstream

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import org.graphstream.algorithm.generator.Generator;
+import org.graphstream.algorithm.generator.WattsStrogatzGenerator;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Graph graph = new SingleGraph("This is a small world!");
+		Generator gen = new WattsStrogatzGenerator(20, 2, 0.5);
 
+		gen.addSink(graph);
+		gen.begin();
+		while(gen.nextEvents()) {}
+		gen.end();
+
+		graph.display(false); // Node position is provided.
 	}
 
 }
