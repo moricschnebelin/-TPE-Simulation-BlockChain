36cec62ab6d20e25c02528cd23bcc5bc147440d2
sm174714 <sm174714@ste007-11.univlehavre.lan>
2018-04-20 12:41:02 +0200

    generation graph primaire

--- src/Initialisation.java
+++ src/Initialisation.java
@@ -1,6 +1,13 @@
 import java.time.Clock;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
+
+import javax.tools.Tool;
+
+import org.graphstream.algorithm.Toolkit;
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Node;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.SingleGraph;
 
@@ -8,8 +15,38 @@ public class Initialisation {
 	
 	static Graph blockchain = new SingleGraph("Blockchain Network");
 	
-	static void PrimeNode() {	//initialisation du graph avec un noeud primaire
-		blockchain.addNode(LocalDate.now(Clock.systemUTC()).toString().concat(":"+LocalTime.now(Clock.systemUTC()).toString()).replaceAll("[-.]", ":"));	//creation du noeud primaire avec un id definit par le temps UTC/GMT de la forme annee:mois:jour:heure:minute:seconde:milliseconde
+	static void PrimeGraph(int numberOfNodes) throws InterruptedException {	//initialisation du graph avec un noeud primaire
+		
+		//création des noeuds
+		for(int i = 0; i < numberOfNodes; i++) {
+
+			blockchain.addNode(LocalDate.now(Clock.systemUTC()).toString().concat(":"+LocalTime.now(Clock.systemUTC()).toString()).replaceAll("[-.]", ":")); // annee:mois:jour:heure:minute:seconde:milliseconde
+			
+			TimeUnit.MILLISECONDS.sleep(1); // attends 1 ms pour avoir un id différent pour chaque noeuds
+		}
+		
+		//liaisons random entre chaques noeuds
+		for(Node n : blockchain){
+			int i = 0;
+			while(i != 2) {
+				boolean isvoisin = false;
+				Node nRand = Toolkit.randomNode(blockchain);
+				for(Edge e : n) {
+					Node voisin = e.getOpposite(n);
+					if(nRand == voisin) {
+						isvoisin = true;
+					}
+					if(isvoisin)
+						break;
+				}
+				if(nRand != n && isvoisin == false) {
+					blockchain.addEdge(LocalDate.now(Clock.systemUTC()).toString().concat(":"+LocalTime.now(Clock.systemUTC()).toString()).replaceAll("[-.]", ":"), n, nRand);
+					i++;
+					TimeUnit.MILLISECONDS.sleep(1); //millisecondes
+				}
+			}
+		}
+		
 	}
 	
 	static Graph GetBlockChain() {	//recuperation du graph
